9adf9c4f24dd735c0f675a0a098d2a7b20dd25de
kunal ladhani <kunal.ladhani@goniyo.com>
2023-04-05 14:19:03 +0530

    added node intro and module notes

--- NodeJS/day-1.js
+++ NodeJS/day-1.js
@@ -1,16 +1,29 @@
-const fileSystem = require('fs');
-// Nodejs has some builtin modules.
-// fs - to work with files
-// http - make http requests
-// os- to work with os
-// etc...
-// which vanilla in-browser JS does not have.
+// Node.js is basically a JS runtime environment
+// it contains V8 engine inside it to run JS + some other functionalities
 
 // helps us write server side code
 // the transpiter had V8 engine embededed in it (written in C++)
-// compilers/parses/interprets the JS code to machine code.
+// transpiles the JS code to machine code that can run on machine.
 
-fileSystem.writeFileSync(`hello.txt`, `Hello World! \nwrote this file using Node.js \nMy name is kunal Ladhani`);
+// TODO: LEARN COMPILER, TRANSPILER, INTERPRETER, PARSER
 
-const fileContent = fileSystem.readFileSync(`./hello.txt`);
-console.log(fileContent.toLocaleString());
+// REPL - read eval print loop
+// we can write js code here and it will run on node.
+// can theoretically write long programs in it
+// but its not convienent so we have other way.
+
+// we write the code in .js or .ts files and run it using node file_name.js 
+
+
+// nodejs has access to all system resources.
+// so we can use nodejs for writing backend on machine
+// it gives us different functionality than the browser JS
+
+// react on frontend
+// node on backend
+// express on server
+// mongo on database
+// full stack can be covered using the JS/TS language only!
+
+console.log("Hello world!");
+//alert("starting NodeJS"); -> alert not defined.
---
+++ NodeJS/day-2.js
@@ -0,0 +1,59 @@
+/* Modules */
+
+// Nodejs has some builtin modules.
+// fs - to work with files
+// http - make http requests
+// os- to work with os
+// http
+// stream
+// etc...
+// which vanilla in-browser JS does not have.
+
+// no need to remember all modules names functionality etc.
+// read the documentation and do it.
+
+// CJS - Common JS
+// way of managing import export of dependencies
+// we can bundle common functionality together under one name.
+const sum = require("./sum");
+// module.exports = sum
+console.log(sum(4, 5));
+
+const os = require('os');
+
+console.log(os.homedir());
+console.log(os.type());
+console.log(os.arch());
+console.log(os.cpus().length);
+console.log(os.userInfo());
+
+
+const fileSystem = require('fs');
+
+const fileContent = fileSystem.readFileSync(`./hello.txt`);
+// assumes all files as a buffer, and reads it like that.
+// Buffer : reads it as a array of bytes
+console.log(fileContent.toLocaleString());
+
+fileSystem.writeFileSync(`hello.txt`, `Hello World! \nwrote this file using Node.js \nMy name is kunal Ladhani`);
+
+const hello = fileSystem.readFileSync(`./hello.txt`, { encoding: "utf-8" });
+console.log(hello);
+// all functions have a xxxSync() - synchronous option available
+// it executes the functionality synchronously => BAD IDEA!
+// sync has BLOCKING I/O so its not usually used.
+
+
+//ESM - ES Modules
+// new way of managing import export of dependencies
+// introduced in ES6
+// like react
+// => import sum from "./sum";
+// => export { sum }
+
+// but you will have to change cofig inside package.json
+// to generate it we need to create new node project
+// npm init
+// will ask basics and you can now have package.json config file.
+
+// IMPORTANT: you are a backend dev, should always have a habit of reading documentation.
